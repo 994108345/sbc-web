b817c86b6d9ee0b6a583acae7f581e6efa2cef97
wenzailong <994108345@qq.com>
2019-04-03 14:45:44 +0800

    添加了文章显示页面

--- src/app/website/menu/webmenu.module.ts
+++ src/app/website/menu/webmenu.module.ts
@@ -1,19 +1,9 @@
 import {NgModule} from '@angular/core';
 import {WebmenuComponent} from './webmenu.component';
 import {
-  ButtonModule,
-  InputTextModule,
-  MenuModule,
-  MessageModule,
   MessageService,
-  MessagesModule,
-  PanelMenuModule,
-  PasswordModule
 } from 'primeng/primeng';
 import {WebmenuRouting} from './webmenu.routing';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ToastModule} from 'primeng/toast';
 import {NgZorroAntdModule, NZ_I18N, NzIconService, zh_CN} from 'ng-zorro-antd';
 import {CommonService} from '../../common/service/base/common.service';
 import {WzlAlertService} from '../../common/service/wzlalert/wzlalert.service';
